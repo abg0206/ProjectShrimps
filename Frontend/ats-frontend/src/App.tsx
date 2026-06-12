ebba1fe554ee772f2a5ef11d0eb60cc122760d21
Andy <andybgonzalez22@hotmail.com>
2026-06-12 11:55:32 -0400

    commit

--- Frontend/ats-frontend/src/App.tsx
+++ Frontend/ats-frontend/src/App.tsx
@@ -1,15 +1,20 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import LoginPage from './pages/LoginPage'
+import DashboardPage from './pages/DashboardPage'
+import ProfilePage from './pages/ProfilePage'
+import SettingsPage from './pages/SettingsPage'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<div>Shrimply</div>} />
-        <Route path="/dashboard" element={<div>Dashboard</div>} />
-        <Route path="/profile" element={<div>Profile</div>} />
-        <Route path="/settings" element={<div>Settings</div>} />
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/settings" element={<SettingsPage />} />
       </Routes>
     </BrowserRouter>
-  );
+  )
 }
-export default App;
+
+export default App
